ea25534c7a904a855862879baf858a611a9068fd
RyanARinger <ringeraryan@gmail.com>
2019-05-02 17:14:32 -0700

    Signed-off-by: RyanARinger <ringeraryan@gmail.com>

---
+++ Projects/CSC18A_ProjectFiles/DeckofCardsV4/DeckOfCardsV2.java
@@ -0,0 +1,34 @@
+/* 
+ * Author: Ryan Ringer
+ * Created on: May 2nd, 2019
+ * File: deckofcardsv4
+ * Purpose: creating a deck of cards
+ */
+
+package deckofcardsv2;
+
+
+public class DeckOfCardsV2 {
+
+    public static void main(String[] args) {
+        // Seed random number or initialize scanner
+        
+
+        // Constant Variables
+        
+
+        // Declare all Variables Here
+        Deck deck = new Deck();
+        
+        // Input or initialize values Here
+
+        // Process/Calculations Here
+        deck.shuffle();
+
+        // Output Located Here
+        deck.showDeck();
+        
+        // Exit
+    }
+
+}
